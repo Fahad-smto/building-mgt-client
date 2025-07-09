e4bdb9a61a245c138f6308b0d1a167076a8460a9
Fahad-smto <simanto.smto.prog@gmail.com>
2025-07-10 01:50:09 +0600

    the project setup is done

--- src/Mainlayouts/MainLayouts.jsx
+++ src/Mainlayouts/MainLayouts.jsx
@@ -2,14 +2,19 @@ import { Outlet } from "react-router";
 import NavBar from "../Pages/NavBar";
 import Footer from "../Pages/Footer";
 
- 
+
 
 const MainLayouts = () => {
     return (
-        <div>
+        <div className="max-w-6xl mx-auto ">
+
             <NavBar></NavBar>
-            <Outlet></Outlet>
+
+            <div className="min-h-[calc(100vh-289px)]">
+                <Outlet></Outlet>
+            </div>
             <Footer></Footer>
+
         </div>
     );
 };
